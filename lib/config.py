69c363febc01f8288c242f4b8b0af799d499fe54
Axujen <axujen@autistici.org>
2016-08-21 14:57:17 +0100

    Add dummy config.py

---
+++ lib/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+# This file contains default configuration options for openmw-mm
+# TODO: Actually write the code for this instead of usign hardcoded values
+
+config = {}
+config["General"] = {}
+config["General"]["mod_dir"] = "./unittests/mods"
+config["General"]["mod_extensions"] = [".esm", ".esp", ".omwaddon"]
